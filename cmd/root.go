fc889cf86701cca56a6734c15fb70809cfe6fb04
dreamjz <25699818+dreamjz@users.noreply.github.com>
2023-11-21 03:30:24 +0800

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,20 @@
 package cmd
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"time"
-	"bytes"
 
 	"github.com/TheZeroSlave/zapsentry"
 	sentry "github.com/getsentry/sentry-go"
 	"github.com/spf13/cobra"
 	"go.keploy.io/server/utils"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/buffer"
+	"go.uber.org/zap/zapcore"
 )
 
 var Emoji = "\U0001F430" + " Keploy:"
@@ -31,7 +31,7 @@ type colorConsoleEncoder struct {
 	*zapcore.EncoderConfig
 	zapcore.Encoder
 }
-  
+
 func NewColorConsole(cfg zapcore.EncoderConfig) (enc zapcore.Encoder) {
 	return colorConsoleEncoder{
 		EncoderConfig: &cfg,
@@ -39,12 +39,12 @@ func NewColorConsole(cfg zapcore.EncoderConfig) (enc zapcore.Encoder) {
 		Encoder: zapcore.NewConsoleEncoder(cfg),
 	}
 }
-  
+
 // EncodeEntry overrides ConsoleEncoder's EncodeEntry
 func (c colorConsoleEncoder) EncodeEntry(ent zapcore.Entry, fields []zapcore.Field) (buf *buffer.Buffer, err error) {
 	buff, err := c.Encoder.EncodeEntry(ent, fields) // Utilize the existing implementation of zap
 	if err != nil {
-		return nil, err	
+		return nil, err
 	}
 
 	bytesArr := bytes.Replace(buff.Bytes(), []byte("\\u001b"), []byte("\u001b"), -1)
@@ -58,30 +58,30 @@ func (c colorConsoleEncoder) Clone() zapcore.Encoder {
 	clone := c.Encoder.Clone()
 	return colorConsoleEncoder{
 		EncoderConfig: c.EncoderConfig,
-		Encoder: clone,
+		Encoder:       clone,
 	}
 }
 
 func init() {
 	_ = zap.RegisterEncoder("colorConsole", func(config zapcore.EncoderConfig) (zapcore.Encoder, error) {
-	  	return NewColorConsole(config), nil
-	}) 
+		return NewColorConsole(config), nil
+	})
 }
 
 func setupLogger() *zap.Logger {
 	logCfg := zap.NewDevelopmentConfig()
-	
+
 	logCfg.Encoding = "colorConsole"
 
 	// Customize the encoder config to put the emoji at the beginning.
 	logCfg.EncoderConfig.EncodeTime = customTimeEncoder
-  logCfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	logCfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
 	logCfg.OutputPaths = []string{
 		"stdout",
 		"./keploy-logs.txt",
 	}
-  
+
 	if debugMode {
 		go func() {
 			defer utils.HandlePanic()
@@ -198,6 +198,7 @@ func (r *Root) execute() {
 		Short:   "Keploy CLI",
 		Example: rootExamples,
 	}
+	rootCmd.CompletionOptions.DisableDefaultCmd = true // disable default completion cmd
 	rootCmd.SetHelpTemplate(rootCustomHelpTemplate)
 
 	rootCmd.PersistentFlags().BoolVar(&debugMode, "debug", false, "Run in debug mode")
